a8bf31e216f5afa1e2d30cd0774d4631d3835375
Omgitsedward <eddietoral16@gmail.com>
2016-08-17 19:09:41 -0700

    Update adc_stats.py

--- adc_stats.py
+++ adc_stats.py
@@ -39,7 +39,7 @@ print "Hey this one is rms"
 print rms
 
 plt.figure(1)
-plt.title('adc data: Antenna %n',antenna)
+plt.title('adc data: Antenna %i',antenna)
 plt.plot(time,ad,'k')
 plt.axis([0,65535,-136,135])
 plt.grid(True)
